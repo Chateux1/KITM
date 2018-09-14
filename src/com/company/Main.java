ff8f41989daf40f936893f0428b6de7d15018a12
Chateux1 <m.satas@gmail.com>
2018-09-14 15:12:57 +0300

    Add input  entering and printing out functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        //Initialise objects
+        Multiplicator multiplicator = new Multiplicator();
+        Scanner readInput = new Scanner(System.in);
+
+        //Get input
+        int input = readInput.nextInt();
+
+        //Set input to class variable for further use
+        multiplicator.setInputNumber(input);
+
+        //Print the input number stored in class to check if working
+        System.out.println(multiplicator.getInputNumber());
     }
 }
--- src/com/company/Multiplicator.java
+++ src/com/company/Multiplicator.java
@@ -1,5 +1,19 @@
 package com.company;
 
 public class Multiplicator {
+
+    private int inputNumber, multiplication;
+
+    public void setInputNumber(int number) {
+        inputNumber = number;
+    }
+
+    public int getInputNumber() {
+        return inputNumber;
+    }
+
+    public Multiplicator() {
+
+    }
 }
 
