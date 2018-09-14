e487ed4fd6fb4a92bc389bc074ec69959f470cf7
Chateux1 <m.satas@gmail.com>
2018-09-14 15:27:37 +0300

    Add funtionalities
    Functionality to print the multiplication line
    Functionality to generate multiplications using for cycle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Set the number of calculations to make
+        int cycleEnd = 20;
+
         //Initialise objects
         Multiplicator multiplicator = new Multiplicator();
         Scanner readInput = new Scanner(System.in);
@@ -18,5 +21,7 @@ public class Main {
 
         //Print the input number stored in class to check if working
         System.out.println(multiplicator.getInputNumber());
+
+        multiplicator.calculateFor(multiplicator.getInputNumber(), cycleEnd);
     }
 }
--- src/com/company/Multiplicator.java
+++ src/com/company/Multiplicator.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Multiplicator {
 
-    private int inputNumber, multiplication;
+    private int inputNumber, multiplication, numberOfIterations;
 
     public void setInputNumber(int number) {
         inputNumber = number;
@@ -10,6 +10,17 @@ public class Multiplicator {
 
     public int getInputNumber() {
         return inputNumber;
+    }
+
+    public void printLine(int base, int multiplicator) {
+
+        System.out.println(base + " * " + multiplicator + " = " + base * multiplicator);
+    }
+    public void calculateFor(int base, int cycleEnd) {
+
+        for(int i = 1; i < cycleEnd; i++) {
+            printLine(base, i);
+        }
     }
 
     public Multiplicator() {
