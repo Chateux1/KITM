8058b2440fb0c1176b37fa3eb972a59411b55c9e
Moksleivis <moksleivis@KITM.local>
2018-10-10 11:24:47 +0300

    Some changes. Working

--- JAVA/cw4/src/com/company/Main.java
+++ JAVA/cw4/src/com/company/Main.java
@@ -8,7 +8,12 @@ public class Main {
         Studentas antanas = new Studentas("Antanas", "Petrauskas");
         Studentas edgaras = new Studentas("Edgaras", 25);
         Studentas paulius = new Studentas("Paulius","Antanavicius","17R-18T");
-
         Table table = new Table(saulius, petras, antanas,edgaras, paulius);
+
+        petras.setPavarde("Aleksandravicius");
+        table = new Table(saulius, petras, antanas,edgaras, paulius);
+
+        edgaras.setGrupe("AAABBBCCCDDD");
+        table = new Table(saulius, petras, antanas,edgaras, paulius);
     }
 }
