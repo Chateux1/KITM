b251c0f858ed053fb9597d4a8e9ed4324304f915
Chateux1 <m.satas@gmail.com>
2018-11-06 12:52:01 +0200

    Add changes

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,13 @@
-let greeting = "This is a variable";
+let variable1 = "Part1";
+let variable2 = "Part2"
 
-console.log(greeting);
+console.log(variable1 + " " + variable2);
+
+let x = 5.3;
+let y = 5.1;
+
+console.log(x + y);
+console.log((x * 10 + y * 10) / 10);
+
+y = '3';
+console.log(x + parseInt(y));
