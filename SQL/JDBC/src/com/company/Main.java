3dc76c2437eac2069b323c2f6fac1e3b3c162e85
Chateux1 <m.satas@gmail.com>
2018-11-21 14:04:41 +0200

    add statements to edit/remove/print selection

--- SQL/JDBC/src/com/company/Main.java
+++ SQL/JDBC/src/com/company/Main.java
@@ -1,32 +1,97 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Employee test = new Employee("Povilas", "Povilaitis", 2246.7);
-
-        String query = "INSERT INTO employees"
-                + "( name, surname, salary )"
-                + "VALUES ( ?, ?, ? )";
 
         String url = "jdbc:mysql://localhost/my_first_database";
 
+//        INSERT A NEW ENTRY TO THE DATABASE
+
+//        String query = "INSERT INTO employees"
+//                + "( name, surname, salary )"
+//                + "VALUES ( ?, ?, ? )";
+//
+//        try {
+//            Connection conn = DriverManager.getConnection(url, "root", "");
+//            PreparedStatement st = ((Connection) conn).prepareStatement(query);
+//            st.setString(1, test.getName());
+//            st.setString(2, test.getSurname());
+//            st.setDouble(3, test.getSalary());
+//
+//            st.executeUpdate();
+//            System.out.println("Naujas irasas iterptas sekmingai");
+//        } catch ( SQLException e ) {
+//            System.out.println("Nepavyko prisijungti prie duomenu bazes");
+//            e.printStackTrace();
+//        }
+
+//        EDIT AN EXISTING ENTRY
+
+//        String query = "UPDATE employees SET salary = ? WHERE id = ?";
+//
+//        try {
+//            Connection conn = DriverManager.getConnection(url, "root", "");
+//            PreparedStatement st = conn.prepareStatement(query);
+//            st.setDouble(1, 333.33);
+//            st.setInt(2, 2);
+//
+//            st.executeUpdate();
+//            st.close();
+//
+//            System.out.println("Irasas sekmingai paredaguotas");
+//
+//        } catch (SQLException e) {
+//            System.out.println("Nepavyko prisijungti prie duomenu bazes");
+//            e.printStackTrace();
+//        }
+
+//        DELETE AN EXISTING ENTRY
+
+//        String query = "DELETE FROM employees WHERE id = ?";
+//
+//        try {
+//            Connection conn = DriverManager.getConnection(url, "root", "");
+//            PreparedStatement st = conn.prepareStatement(query);
+//            st.setInt(1, 2);
+//
+//            st.executeUpdate();
+//            st.close();
+//
+//            System.out.println("Irasas sekmingai istrintas");
+//
+//        } catch (SQLException e) {
+//            System.out.println("Nepavyko prisijungti prie duomenu bazes");
+//            e.printStackTrace();
+//        }
+
+//        PRINT SELECTION
+
+        String query = "SELECT * FROM employees";
+
         try {
             Connection conn = DriverManager.getConnection(url, "root", "");
-            PreparedStatement st = ((Connection) conn).prepareStatement(query);
-            st.setString(1, test.getName());
-            st.setString(2, test.getSurname());
-            st.setDouble(3, test.getSalary());
 
-            st.executeUpdate();
-            System.out.println("Naujas irasas iterptas sekmingai");
-        } catch ( SQLException e ) {
+            PreparedStatement st = conn.prepareStatement(query);
+//          Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            System.out.println("");
+
+            while (rs.next()) {
+                System.out.print(rs.getString("id") + " ");
+                System.out.print(rs.getString("name") + " ");
+                System.out.print(rs.getString("surname") + " ");
+                System.out.print(rs.getString("salary") + " ");
+                System.out.print(rs.getString("data") + "\n");
+            }
+            st.close();
+
+        } catch (SQLException e) {
             System.out.println("Nepavyko prisijungti prie duomenu bazes");
             e.printStackTrace();
         }
