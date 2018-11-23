a61cefad5ae5068e7181f21a4f0c11bab676f9a7
Chateux1 <m.satas@gmail.com>
2018-11-23 11:26:52 +0200

    split script to separate functions

--- JS/script7.js
+++ JS/script7.js
@@ -1,19 +1,36 @@
 let apiUrl = 'http://kitmjavascript.azurewebsites.net/api/studentai';
+let apiUrlOnlyPost = 'http://kitmjavascript.azurewebsites.net/api/login';
 let myParagraph = $('#myParagraph');	
-$.ajax({
-    url: apiUrl,
-    type: 'GET',
-    crossDomain: true,
-    success: function(response) {
-        responseData = JSON.parse(response);
-        
-        myParagraph.text(responseData[0].name);
-        //console.log(responseData[0]);
-    },
-    error: function() {
-        alert('Fail!');
-    },
-    complete: function() {
-        // happens everytime whether there is an error or not from the server.
-    }
-});
+
+
+function myGetFunction() {
+    $.ajax({
+        url: apiUrl,
+        type: 'GET',
+        crossDomain: true,
+        success: function(response) {
+            responseData = JSON.parse(response);
+            
+            myParagraph.text(responseData[0].name);
+            //console.log(responseData[0]);
+        },
+        error: function() {
+            alert('Fail!');
+        },
+        complete: function() {
+            // happens everytime whether there is an error or not from the server.
+        }
+    });
+};
+
+function myPostFunction() {
+    $.ajax({
+        url: apiUrlOnlyPost,
+        type: 'POST',
+        crossDomain: true,
+    });
+};
+
+myGetFunction();
+
+
