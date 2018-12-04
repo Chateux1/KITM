91e4ad3618d95b13f446a4d12286d8cc7a9b2a6f
Chateux1 <m.satas@gmail.com>
2018-12-04 10:45:29 +0200

    add line to hide input form on successful login

--- JS/atsiskaitymas/script.js
+++ JS/atsiskaitymas/script.js
@@ -1,5 +1,6 @@
 let urlGet = "http://kitmjavascript.azurewebsites.net/api/studentai";
 let urlPost = "http://kitmjavascript.azurewebsites.net/api/login";
+let input = document.getElementsByClassName("input")[0];
 
 $(".input-button > button").on("click", function() {
   let inputName = $(".input-username > input").val();
@@ -15,6 +16,7 @@ $(".input-button > button").on("click", function() {
 
     success: function(response) {
       if (response.isSuccessStatusCode == true) {
+        input.style.display = "none";
         alert("Success!!!");
       } else if (response.isSuccessStatusCode == false) {
         alert("Error :(");
