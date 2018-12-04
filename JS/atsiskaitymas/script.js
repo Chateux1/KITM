0ee959bdb700abd1f493ed1e8a3a48a295cc4af9
Chateux1 <m.satas@gmail.com>
2018-12-04 12:58:11 +0200

    add functions to create header row and empty table with header row

--- JS/atsiskaitymas/script.js
+++ JS/atsiskaitymas/script.js
@@ -1,6 +1,7 @@
 let urlGet = "http://kitmjavascript.azurewebsites.net/api/studentai";
 let urlPost = "http://kitmjavascript.azurewebsites.net/api/login";
 let input = document.getElementsByClassName("input")[0];
+let output = document.getElementsByClassName("output")[0];
 
 $(".input-button > button").on("click", function() {
   let inputName = $(".input-username > input").val();
@@ -17,10 +18,38 @@ $(".input-button > button").on("click", function() {
     success: function(response) {
       if (response.isSuccessStatusCode == true) {
         input.style.display = "none";
-        alert("Success!!!");
+        // alert("Success!!!");
       } else if (response.isSuccessStatusCode == false) {
         alert("Error :(");
       }
     }
   });
 });
+
+function setHeaderRow() {
+  let headerLabels = [
+    "name",
+    "age",
+    "picture",
+    "balance",
+    "tags",
+    "registered",
+    "favoriteFruit"
+  ];
+  let headerString = "<tr>";
+  headerLabels.forEach(element => {
+    headerString += "<th>" + element + "</th>";
+  });
+  headerString += "</tr>";
+  return headerString;
+}
+function setEmptyTable() {
+  let table = "<table class='table'></table>";
+  let headers = setHeaderRow();
+  //   alert(headers);
+
+  $(".output").append(table);
+  $(".table").append(headers);
+}
+
+setEmptyTable();
